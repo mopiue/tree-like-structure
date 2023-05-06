97fe850eff9b3dcae62dcf170e214919d0b3d451
IamNucassi <nucassi@bk.ru>
2023-05-06 16:17:05 +0300

    wip: testing new structure of nodes data

--- src/components/NodeElement/NodeElement.jsx
+++ src/components/NodeElement/NodeElement.jsx
@@ -1,12 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addNode } from '../../features/nodesSlice'
 import styles from './NodeElement.module.scss'
+import { useEffect } from 'react'
 
 function NodeElement({ nestingLevel, node }) {
+  const dispatch = useDispatch()
+  const nodes = useSelector((state) => state.tree.nodes)
+
+  const handleClick = (node) => {
+    dispatch(addNode(node))
+  }
+
   return (
     <span className={styles.NodeElement}>
       <li
         style={{
           marginLeft: nestingLevel * 25,
         }}
+        onClick={() => handleClick(node)}
       >
         {node}
       </li>
--- src/features/nodesSlice.js
+++ src/features/nodesSlice.js
@@ -4,10 +4,27 @@ const initialState = {
   nodes: [['Node 1', ['Node 2', ['Node 3'], ['Node 4']], ['Node 5']]],
 }
 
+const findNodeToInteract = (arr, node) => {
+  const rand = Math.round(Math.random() * 10000)
+  arr.forEach((el) => {
+    if (Array.isArray(el)) {
+      findNodeToInteract(el, node)
+      if (el[0] === node) el.push([`${rand}`])
+    }
+  })
+  return arr
+}
+
 export const nodesSlice = createSlice({
   name: 'tree',
   initialState,
-  reducers: {},
+  reducers: {
+    addNode: (state, action) => {
+      const updatedNodes = findNodeToInteract(state.nodes, action.payload)
+      state.nodes = updatedNodes
+    },
+  },
 })
 
+export const { addNode } = nodesSlice.actions
 export default nodesSlice.reducer
